db55d812b2b9252b329e226ddfc852ea4712817c
JoeTakasuna <jonpili99@gmail.com>
2020-04-14 16:22:03 +0900

    change template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Home
+  },
+  // data () {
+  //   return {
+  //     xxx: データの置き場所
+  //   }
+  // },
+  // props: {
+  //   xxx: データの受け取り
+  // },
+  // computed: {
+  //   xxx () {
+  //     return データの自動算出
+  //   }
+  // },
+  // watch: {
+  //   xxx () {
+  //     データ変更時にメソッドを実行
+  //   }
+  // },
+  // methods: {
+  //   xxx () {
+  //     メソッドの組み込み
+  //   }
+  // },
+  // created () {
+  //   インスタンスが作成された後に同期的に呼ばれるフック
+  // },
+  // mounted () {
+  //   インスタンスがマウントされたちょうど後に呼ばれるフック
+  // }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .home
+    h1 {{ msg }}
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    margin: 40px 0 0;
+  }
+</style>
