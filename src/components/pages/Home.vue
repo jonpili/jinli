d018295df10fe51e5475ab3ca6bf940099584761
JoeTakasuna <jonpili99@gmail.com>
2020-04-14 19:56:16 +0900

    tmp

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
   el-container
-    el-header header
+    el-header
+      el-button(icon="el-icon-notebook-2", type="success", circle)
+      span.fs-500.fw-bold.ml-500 jinli
+      hr(size="1", color="grey")
     el-main main
 </template>
 
@@ -13,4 +16,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .project-icon-area {
+    @include m-fixed-width(48px)
+    @include m-fixed-height(48px)
+    background-color: black;
+    border-radius: $basespace-200;
+  }
+  .project-icon {
+    width: 100%;
+    color: white;
+  }
 </style>
