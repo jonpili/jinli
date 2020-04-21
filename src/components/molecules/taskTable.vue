8a160e079e57c8904ec3624f88c84d109ec2a52d
Joe.Takasuna <jonpili99@gmail.com>
2020-04-21 18:04:32 +0900

    ドラッグできそうなボタンを追加

--- src/components/molecules/taskTable.vue
+++ src/components/molecules/taskTable.vue
@@ -3,6 +3,9 @@
            :header-cell-style="{height: '32px', padding: '0'}",
            :cell-style="{padding: '0'}",
            border)
+    el-table-column(width="32")
+      .move-icon-area
+        v-icon.move-icon(name="grip-vertical")
     el-table-column(v-for="column in columns", :key="column.id", :label="column.label", :width="column.width")
       template(slot-scope="scope")
         el-input(v-model="scope.row[column.value]")
@@ -16,3 +19,21 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .move-icon-area {
+    padding-left: 11px;
+    padding-top: 7px;
+    opacity: 0;
+    &:hover {
+      opacity: 1;
+      cursor: grab;
+    }
+    &:active {
+      cursor: grabbing;
+    }
+  }
+  .move-icon {
+    color: $bdcolor-dark;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ja'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, { locale })
+Vue.component('v-icon', Icon)
 
 new Vue({
   render: h => h(App),
