e815522a12215440e9626092b1d094938bdf9a09
JoeTakasuna <jonpili99@gmail.com>
2020-04-22 15:22:43 +0900

    jMoveIconを切り出し2

---
+++ src/components/atoms/jMoveIcon.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  span.move-icon-area
+    v-icon.move-icon(name="grip-vertical")
+</template>
+
+<style lang="scss" scoped>
+  .move-icon-area {
+    margin: 0 $basespace-200;
+    vertical-align: middle;
+    &:hover {
+      cursor: grab;
+    }
+    &:active {
+      cursor: grabbing;
+    }
+  }
+  .move-icon {
+    color: $bdcolor-dark;
+  }
+  ::v-deep .el-input__inner {
+    height: $basespace-600;
+    border-radius: 0px;
+  }
+</style>
