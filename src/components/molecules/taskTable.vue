77d0b9294d2691e26f4c7f6943a1adf949578b28
JoeTakasuna <jonpili99@gmail.com>
2020-04-22 15:27:02 +0900

    不使用なCSSの削除

--- src/components/molecules/taskTable.vue
+++ src/components/molecules/taskTable.vue
@@ -3,9 +3,8 @@
     draggable
       el-input.header(v-for="column in columns", v-model="column.label", :style="{ width: column.width + 'px' }", readonly)
       div(v-for="row in data")
-        .row-area
-          j-move-icon
-          el-input(v-for="column in columns", v-model="row[column.value]", :style="{ width: column.width + 'px' }")
+        j-move-icon
+        el-input(v-for="column in columns", v-model="row[column.value]", :style="{ width: column.width + 'px' }")
 </template>
 
 <script>
@@ -28,10 +27,6 @@ export default {
   .header ::v-deep .el-input__inner {
     margin-left: 34px;
     border: none;
-  }
-  .row-area {
-    width: 100%;
-    overflow: scroll;
   }
   ::v-deep .el-input__inner {
     height: $basespace-600;
