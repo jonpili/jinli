5444eaa9c86662b70885e788e10f81002c2db260
JoeTakasuna <jonpili99@gmail.com>
2020-04-24 13:57:38 +0900

    詳細表示ボタンの設置

--- src/components/molecules/taskTable.vue
+++ src/components/molecules/taskTable.vue
@@ -1,12 +1,17 @@
 <template lang="pug">
   div.pl-600
-    el-input.header(v-for="column in columns", :key="column.id", v-model="column.label", :style="{ width: column.width + 'px' }", readonly)
+    el-input.header(v-model="columns[0].label", :style="{ width: columns[0].width + 'px' }", readonly)
+    .open-detail-modal-button-area
+    el-input.header(v-for="column in columns.slice(1)", :key="column.id", v-model="column.label", :style="{ width: column.width + 'px' }", readonly)
     draggable(group="tasks")
       transition-group(name="task-list", tag="div")
         .task-list-item(v-for="row in data", :key="row.id")
           j-move-icon
           j-complete-button(@click="completeTask(row.id)")
-          el-input(v-for="column in columns", :key="column.id", v-model="row[column.value]", :style="{ width: column.width + 'px' }")
+          el-input(v-model="row[columns[0].value]", :style="{ width: columns[0].width + 'px' }")
+          .open-detail-modal-button-area
+            span 詳細 >
+          el-input(v-for="column in columns.slice(1)", :key="column.id", v-model="row[column.value]", :style="{ width: column.width + 'px' }")
 </template>
 
 <script>
@@ -63,5 +68,16 @@ export default {
   .task-list-leave-to {
     opacity: 0;
     transform: translateX($basespace-500 * 10);
+  }
+  .open-detail-modal-button-area {
+    display: inline-block;
+    text-align: center;
+    width: $basespace-600 * 2;
+    font-size: $basespace-200;
+    cursor: pointer;
+    opacity: 0;
+    &:hover {
+      opacity: 1;
+    }
   }
 </style>
